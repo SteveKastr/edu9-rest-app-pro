664fc5d9750489dfbfeeb8f36127df62ff8589f4
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-12 01:26:42 +0300

    Initial commit

---
+++ src/test/java/gr/aueb/cf/edu9app/EduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.edu9app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
