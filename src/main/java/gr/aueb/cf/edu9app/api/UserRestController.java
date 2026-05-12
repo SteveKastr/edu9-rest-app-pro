da5b6b676a043cf978a67c0b081be52eb4fd5191
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-12 20:00:58 +0300

    feat: add user controller for save user

---
+++ src/main/java/gr/aueb/cf/edu9app/api/UserRestController.java
@@ -0,0 +1,42 @@
+package gr.aueb.cf.edu9app.api;
+
+import gr.aueb.cf.edu9app.core.exception.EntityAlreadyExistsException;
+import gr.aueb.cf.edu9app.core.exception.EntityInvalidArgumentException;
+import gr.aueb.cf.edu9app.core.exception.ValidationException;
+import gr.aueb.cf.edu9app.dto.UserInsertDTO;
+import gr.aueb.cf.edu9app.dto.UserReadOnlyDTO;
+import gr.aueb.cf.edu9app.service.IUserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserReadOnlyDTO> registerUser(@Valid @RequestBody UserInsertDTO userInsertDTO,
+                                                        BindingResult bindingResult)
+            throws ValidationException, EntityAlreadyExistsException, EntityInvalidArgumentException {
+        // TODO implement validator for business rules
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("User", "Invalid user data", bindingResult);
+        }
+        UserReadOnlyDTO userReadOnlyDTO = userService.saveUser(userInsertDTO);
+
+        URI location = URI.create("/api/v1/users/" + userReadOnlyDTO.uuid());
+        return ResponseEntity.created(location).body(userReadOnlyDTO);
+
+    }
+}
