822c43617177567ccd7bd39df55e525753a19238
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-12 18:49:48 +0300

    chore: add entity not found exception

--- src/main/java/gr/aueb/cf/edu9app/service/IUserService.java
+++ src/main/java/gr/aueb/cf/edu9app/service/IUserService.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.edu9app.service;
 
 import gr.aueb.cf.edu9app.core.exception.EntityAlreadyExistsException;
 import gr.aueb.cf.edu9app.core.exception.EntityInvalidArgumentException;
+import gr.aueb.cf.edu9app.core.exception.EntityNotFoundException;
 import gr.aueb.cf.edu9app.dto.UserInsertDTO;
 import gr.aueb.cf.edu9app.dto.UserReadOnlyDTO;
 
@@ -11,6 +12,6 @@ public interface IUserService {
     UserReadOnlyDTO saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException,
             EntityInvalidArgumentException;
 
-    UserReadOnlyDTO getUserByUUID(UUID uuid);
-    UserReadOnlyDTO getUserByUUIDDeletedFalse(UUID uuid);
+    UserReadOnlyDTO getUserByUUID(UUID uuid) throws EntityNotFoundException;
+    UserReadOnlyDTO getUserByUUIDDeletedFalse(UUID uuid) throws EntityNotFoundException;
 }
