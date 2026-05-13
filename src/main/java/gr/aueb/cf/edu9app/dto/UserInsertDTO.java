881ca7e4abf8404309bf83334c15784622ca9ee4
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-14 00:15:42 +0300

    fix: change @NotBlank to @NotNull for roleid

--- src/main/java/gr/aueb/cf/edu9app/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/edu9app/dto/UserInsertDTO.java
@@ -14,7 +14,7 @@ public record UserInsertDTO(
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
         String password,
 
-        @NotBlank
+        @NotNull
         Long roleId
 ) {
 }
