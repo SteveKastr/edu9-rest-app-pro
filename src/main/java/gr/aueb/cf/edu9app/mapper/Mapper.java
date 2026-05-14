c41558c2e2eb4330336bce5ecc7df130c794d557
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-14 21:23:12 +0300

    chore: insert into mapper teacher mapping for read only dto and teacher insert dto to teacher entity

--- src/main/java/gr/aueb/cf/edu9app/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/edu9app/mapper/Mapper.java
@@ -1,7 +1,8 @@
 package gr.aueb.cf.edu9app.mapper;
 
-import gr.aueb.cf.edu9app.dto.UserInsertDTO;
-import gr.aueb.cf.edu9app.dto.UserReadOnlyDTO;
+import gr.aueb.cf.edu9app.dto.*;
+import gr.aueb.cf.edu9app.model.PersonalInfo;
+import gr.aueb.cf.edu9app.model.Teacher;
 import gr.aueb.cf.edu9app.model.User;
 import org.springframework.stereotype.Component;
 
@@ -16,4 +17,34 @@ public class Mapper {
         return new UserReadOnlyDTO(user.getUuid().toString(), user.getUsername(),
                 user.getRole().getName());
     }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        return new TeacherReadOnlyDTO(teacher.getUuid().toString(), teacher.getFirstname(), teacher.getLastname(),
+                teacher.getVat(), teacher.getRegion().getName());
+    }
+    public Teacher mapToTeacherEntity(TeacherInsertDTO dto) {
+        Teacher teacher = new Teacher();
+        teacher.setFirstname(dto.firstname());
+        teacher.setLastname(dto.lastname());
+        teacher.setVat(dto.vat());
+
+        UserInsertDTO userDTO = dto.userInsertDTO();
+        User user = new User();
+        user.setUsername(userDTO.username());
+        user.setPassword(userDTO.password());
+
+        teacher.addUser(user);
+
+        PersonalInfoInsertDTO personalInfoDTO = dto.personalInfoInsertDTO();
+        PersonalInfo personalInfo = new PersonalInfo();
+        personalInfo.setAmka(personalInfoDTO.amka());
+        personalInfo.setIdentityNumber(personalInfoDTO.identityNumber());
+        personalInfo.setPlaceOfBirth(personalInfoDTO.placeOfBirth());
+        personalInfo.setMunicipalityOfRegistration(personalInfoDTO
+                .municipalityOfRegistration());
+        teacher.setPersonalInfo(personalInfo);  // Set PersonalInfo entity to Teacher
+
+        return teacher;
+    }
+
 }
