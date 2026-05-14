c3098a2887c28c88692496306d9ae69308963c1f
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-14 21:25:57 +0300

    chore: add preauthorize to get user by uuid

--- src/main/java/gr/aueb/cf/edu9app/service/UserService.java
+++ src/main/java/gr/aueb/cf/edu9app/service/UserService.java
@@ -1,8 +1,8 @@
 package gr.aueb.cf.edu9app.service;
 
-import gr.aueb.cf.edu9app.core.exception.EntityAlreadyExistsException;
-import gr.aueb.cf.edu9app.core.exception.EntityInvalidArgumentException;
-import gr.aueb.cf.edu9app.core.exception.EntityNotFoundException;
+import gr.aueb.cf.edu9app.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.edu9app.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.edu9app.core.exceptions.EntityNotFoundException;
 import gr.aueb.cf.edu9app.dto.UserInsertDTO;
 import gr.aueb.cf.edu9app.dto.UserReadOnlyDTO;
 import gr.aueb.cf.edu9app.mapper.Mapper;
