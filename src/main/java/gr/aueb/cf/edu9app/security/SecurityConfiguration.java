33796c0ab47069f581bc74c94e3bdeee665b833e
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-19 00:45:52 +0300

    chore: clear

--- src/main/java/gr/aueb/cf/edu9app/security/SecurityConfiguration.java
+++ src/main/java/gr/aueb/cf/edu9app/security/SecurityConfiguration.java
@@ -54,6 +54,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(req -> req
                         .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/teachers").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/users/*").hasAuthority("VIEW_USER")
                         .anyRequest().authenticated()
                 )
